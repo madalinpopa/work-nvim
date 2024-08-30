58993272a57906224420163ced987c6fb421e980
Madalin Popa <contact@madalinpopa.com>
2024-08-30 10:18:24 +0300

    Add files via upload

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.textwidth = 119
+vim.wo.colorcolumn = "120"
---
+++ after/ftplugin/python.lua
@@ -0,0 +1,4 @@
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+vim.bo.textwidth = 88
+vim.wo.colorcolumn = "88"
