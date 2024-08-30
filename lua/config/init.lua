da1d48ab1f30736fb9998097fcc43ab8ac72cf37
Madalin Popa <contact@madalinpopa.com>
2024-08-30 10:14:45 +0300

    Create init.lua

---
+++ lua/config/init.lua
@@ -0,0 +1,7 @@
+------------------------------------------------------
+-- Author: Madalin Popa
+-- Email: contact@madalinpopa.com
+------------------------------------------------------
+require('config.options')
+require('config.lazy')
+require('config.keymap')
